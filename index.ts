51389ff57a93aef3058f714f75fe25dd5f40aa55
suyeon <sroovy@naver.com>
2023-04-12 22:50:28 +0900

    Connect MongoDB

--- index.ts
+++ index.ts
@@ -1,7 +1,17 @@
 import express, { Express, Request, Response } from 'express';
+import mongoose from 'mongoose';
 
 const app: Express = express();
 const port = 5000
+
+const uri = 'mongodb+srv://sroovy:dltndus1@boilerplate.shur2ov.mongodb.net/?retryWrites=true&w=majority';
+  mongoose.connect(uri)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
